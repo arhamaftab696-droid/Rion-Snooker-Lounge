27de3cc9817ac26398e007abb0bcab7b52367c0d
Arham Aftab <arham@example.com>
2026-08-27 11:10:53 +0500

    Host full feature-rich application interface inside native iOS app with Face ID

--- RionSnookeriOS/RionSnookerApp.swift
+++ RionSnookeriOS/RionSnookerApp.swift
@@ -3,31 +3,19 @@ import SwiftUI
 @main
 struct RionSnookerApp: App {
     @StateObject private var auth = BiometricAuth.shared
-    @StateObject private var db = DatabaseManager.shared
-
-    init() {
-        // Dark theme navigation bar and tab bar styling
-        let navBarAppearance = UINavigationBarAppearance()
-        navBarAppearance.configureWithOpaqueBackground()
-        navBarAppearance.backgroundColor = UIColor(red: 2/255, green: 6/255, blue: 23/255, alpha: 1)
-        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
-        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-        UINavigationBar.appearance().standardAppearance = navBarAppearance
-        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
-
-        let tabBarAppearance = UITabBarAppearance()
-        tabBarAppearance.configureWithOpaqueBackground()
-        tabBarAppearance.backgroundColor = UIColor(red: 15/255, green: 23/255, blue: 42/255, alpha: 1)
-        UITabBar.appearance().standardAppearance = tabBarAppearance
-        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
-    }
 
     var body: some Scene {
         WindowGroup {
-            if auth.isUnlocked {
-                MainTabView()
-            } else {
-                LockView()
+            ZStack {
+                Color(red: 2/255, green: 6/255, blue: 23/255)
+                    .ignoresSafeArea()
+
+                if auth.isUnlocked {
+                    NativeWebView()
+                        .ignoresSafeArea(.keyboard, edges: .bottom)
+                } else {
+                    LockView()
+                }
             }
         }
     }
---
+++ RionSnookeriOS/Views/NativeWebView.swift
@@ -0,0 +1,60 @@
+import SwiftUI
+import WebKit
+
+public struct NativeWebView: UIViewRepresentable {
+    public let url: URL
+
+    public init(url: URL = URL(string: "https://rion-snooker-lounge-rk51.onrender.com")!) {
+        self.url = url
+    }
+
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    public func makeUIView(context: Context) -> WKWebView {
+        let config = WKWebViewConfiguration()
+        config.allowsInlineMediaPlayback = true
+        config.mediaTypesRequiringUserActionForPlayback = []
+        config.preferences.javaScriptCanOpenWindowsAutomatically = true
+
+        let webView = WKWebView(frame: .zero, configuration: config)
+        webView.navigationDelegate = context.coordinator
+        webView.isOpaque = false
+        webView.backgroundColor = UIColor(red: 2/255, green: 6/255, blue: 23/255, alpha: 1)
+        webView.scrollView.backgroundColor = UIColor(red: 2/255, green: 6/255, blue: 23/255, alpha: 1)
+
+        // Add native iOS Pull-to-Refresh
+        let refreshControl = UIRefreshControl()
+        refreshControl.tintColor = UIColor(red: 16/255, green: 185/255, blue: 129/255, alpha: 1)
+        refreshControl.addTarget(context.coordinator, action: #selector(Coordinator.handleRefresh(_:)), for: .valueChanged)
+        webView.scrollView.refreshControl = refreshControl
+
+        let request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 30)
+        webView.load(request)
+        return webView
+    }
+
+    public func updateUIView(_ uiView: WKWebView, context: Context) {}
+
+    public class Coordinator: NSObject, WKNavigationDelegate {
+        var parent: NativeWebView
+
+        init(_ parent: NativeWebView) {
+            self.parent = parent
+        }
+
+        @objc func handleRefresh(_ sender: UIRefreshControl) {
+            sender.endRefreshing()
+            // Reload page on pull-down
+            if let webView = sender.superview as? UIScrollView, let parentView = webView.superview as? WKWebView {
+                parentView.reload()
+            }
+        }
+
+        public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            // Smooth background match
+            webView.evaluateJavaScript("document.body.style.backgroundColor = '#020617';", completionHandler: nil)
+        }
+    }
+}
